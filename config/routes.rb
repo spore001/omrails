430e4290563c10cde7a33f2802655313e0513e74
por <porgrammers@gmail.com>
2018-03-01 17:13:24 +0700

    Add User with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  as :user do
+    get 'signin', to: 'devise/sessions#new'
+    delete 'signout', to: 'devise/sessions#destroy'
+    get 'signup', to: 'devise/registrations#new'
+  end
   get 'pages/home'
   get 'about' => 'pages#about'
 
