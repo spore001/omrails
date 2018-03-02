bf863b49f234b716469266d62c3b95bcf13e9ebc
por <porgrammers@gmail.com>
2018-03-02 10:05:32 +0700

    update custom bootrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
